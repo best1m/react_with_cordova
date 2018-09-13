137b4457a50d2338fa360379dbecff275a60f4a9
kth <kthiswell@gmail.com>
2018-09-13 22:11:03 +0900

    function call test

--- platforms/ios/pdlc_controller.xcarchive/Products/Applications/pdlc_controller.app/www/js/components/App.js
+++ platforms/ios/pdlc_controller.xcarchive/Products/Applications/pdlc_controller.app/www/js/components/App.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+    state = {
+        test : null
+    }
+    test = () => {
+        this.setState({
+            test : '21312321'
+        })
+    }
     render(){
         return(
         <div>
-            <h1>react starter!</h1>
+            <h1>pdlc controller</h1>
+            <button onclick={this.test}>TYPE1</button>
+            <button>TYPE2</button>
+            <button>TYPE3</button>
+            <button>TYPE4</button>
+            {this.state.test}
         </div>
         )
     }
--- www/js/components/App.js
+++ www/js/components/App.js
@@ -2,12 +2,23 @@ import React, {Component} from 'react';
 
 class App extends Component {
     state = {
-        test : null
+        test : 2132131
     }
+    callTest = () => {
+        this.setState({
+            test : 2342342323
+        })
+    }
+
     render(){
         return(
         <div>
-            <h1>react starter!</h1>
+            <h1>pdlc controller</h1>
+            <button onClick={this.callTest}>TYPE1</button>
+            <button>TYPE2</button>
+            <button>TYPE3</button>
+            <button>TYPE4</button>
+            <div>{this.state.test}</div>
         </div>
         )
     }
